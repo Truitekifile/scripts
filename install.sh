ba2fd1257692db355c2bd23fa55eb80db6fde5c5
Truitekifile <91056971+Truitekifile@users.noreply.github.com>
2023-10-25 22:52:50 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+echo "installation des prérequis"
+echo
+sudo apt update
+apt install mpv -y
+echo ""
+echo "Creation du répertoir scripts"
+mkdir scripts
+cd /scripts
+
+echo
+echo "Obtention de hals.sh"
+wget https://raw.githubusercontent.com/Truitekifile/scripts/main/halt.sh
+
+echo
+echo "Obtention de USB0.sh"
+wget https://raw.githubusercontent.com/Truitekifile/scripts/main/usb0.sh
+
+echo
+echo "Obtention de USB1.sh"
+wget https://raw.githubusercontent.com/Truitekifile/scripts/main/usb1.sh
+
+echo
+echo "Obtention de video.sh"
+wget https://raw.githubusercontent.com/Truitekifile/scripts/main/vido.sh
+
+
+chmod 777 *
+
+echo
+echo "Terminé"
