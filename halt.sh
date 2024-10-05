33c8e4e60b65c6ef439cfeb07bda568e54c5c436
Truitekifile <91056971+Truitekifile@users.noreply.github.com>
2024-10-05 11:33:31 +0200

    Delete halt.sh

--- halt.sh
+++
@@ -1,9 +0,0 @@
-echo "Attention cela terminera par la meme occasion votre liaison ssh, etes vous sur de vouloir continuer ? "
-read input
-
-if [ "$input" = "o" ]; then
-	halt
-
-else
-	echo "Annulation"
-fi
